a2b13dd38f2b18ed4f360dca7fc35f88ba2beec3
Fainajabi <fainaj5shaikh@gmail.com>
2017-05-11 07:48:51 +0000

    Git First Commit

--- server.js
+++ server.js
@@ -2,10 +2,32 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
-app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost:27017/mittens');
+app.use(bodyParser.json());mongoose.connect('mongodb://localhost:27017/mittens');
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
 
+
+app.use(express.static('public'));
+
+var Meow = mongoose.model('Meow', {
+ text: String
+  });
+
+app.post('/meows', function(req, res, next){	
+		var newMeow = new Meow({
+			text: req.body.newMeow		
+		});
+		newMeow.save(function(err){
+			return res.send("Added Successfully");
+	});	
+	});
+
+
+app.get('/meows', function(req, res, next){
+  Meow.find({}, function(err, meows){
+    return res.json(meows);
+});
+});
